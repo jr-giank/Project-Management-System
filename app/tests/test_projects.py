e68508f7a9169c88dfa8e61a07507fb7f2505ffe
Gianfranco De Paula <baciliogianfranco@gmail.com>
2025-11-16 22:14:14 -0400

    [UPDATE] Tests

--- app/tests/test_projects.py
+++ app/tests/test_projects.py
@@ -2,15 +2,36 @@
 import json
 
 from app.models.projects import Project
+from app.models.users import User
 from app.models.tasks import Task
+from app.routes.auth import create_auth_token
+
+def get_token(session):
+
+    manager_user = session.query(User).filter_by(email="manager@example.com").first()
+    if not manager_user:
+        manager_user = User(
+            first_name="Manager",
+            last_name="User",
+            email="manager@example.com",
+            role="manager",
+            password="SecureP@ssword1"
+        )
+        session.add(manager_user)
+        session.commit()
+
+    token = create_auth_token(manager_user)
+
+    return token
 
 def test_create_project(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     data = {
         "name": "Project #1",
         "description": "Test project description"
     }
 
-    response = client.post("/api/projects", data=json.dumps(data), content_type="application/json")
+    response = client.post("/api/projects", data=json.dumps(data), headers=headers, content_type="application/json")
 
     assert response.status_code == 201
     payload = response.get_json()
@@ -22,57 +43,63 @@ def test_create_project(client, db_session):
     assert project.name == "Project #1"
 
 
-def test_create_project_missing_field(client):
+def test_create_project_missing_field(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     data = {"description": "Missing name"}
-    response = client.post("/api/projects", data=json.dumps(data), content_type="application/json")
+    response = client.post("/api/projects", data=json.dumps(data), headers=headers, content_type="application/json")
     assert response.status_code == 400
     assert "Missing field" in response.get_json()["error"]
 
 
 def test_list_projects(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     db_session.add_all([
         Project(name="Project #1", description="desc"),
     ])
     db_session.commit()
 
-    response = client.get("/api/projects")
+    response = client.get("/api/projects", headers=headers)
     assert response.status_code == 200
     payload = response.get_json()
-    assert isinstance(payload, list)
-    assert any(p["name"] == "Project #1" for p in payload)
+    assert isinstance(payload['data'], list)
+    assert any(p["name"] == "Project #1" for p in payload['data'])
 
 
 def test_get_project(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     project = Project(name="Project #1", description="desc")
     db_session.add(project)
     db_session.commit()
 
-    response = client.get(f"/api/projects/{project.id}")
+    response = client.get(f"/api/projects/{project.id}", headers=headers)
     assert response.status_code == 200
     payload = response.get_json()
     assert payload["name"] == "Project #1"
     assert payload["description"] == "desc"
 
 
-def test_get_project_not_found(client):
-    response = client.get("/api/projects/100")
+def test_get_project_not_found(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.get("/api/projects/100", headers=headers)
     assert response.status_code == 404
     assert response.get_json()["error"] == "Project not found"
 
 
-def test_get_project_invalid_id(client):
-    response = client.get("/api/projects/abc")
+def test_get_project_invalid_id(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.get("/api/projects/abc", headers=headers)
     assert response.status_code == 400
     assert response.get_json()["error"] == "Invalid ID format"
 
 
 def test_update_project_success(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     project = Project(name="Old Project", description="Old desc")
     db_session.add(project)
     db_session.commit()
 
     data = {"name": "New Project", "description": "Updated desc"}
-    response = client.put(f"/api/projects/{project.id}", data=json.dumps(data), content_type="application/json")
+    response = client.put(f"/api/projects/{project.id}", data=json.dumps(data), headers=headers,  content_type="application/json")
 
     assert response.status_code == 200
     payload = response.get_json()
@@ -81,43 +108,49 @@ def test_update_project_success(client, db_session):
     assert updated.name == "New Project"
 
 
-def test_update_project_not_found(client):
+def test_update_project_not_found(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     data = {"name": "Does not exist"}
-    response = client.put("/api/projects/999", data=json.dumps(data), content_type="application/json")
+    response = client.put("/api/projects/999", data=json.dumps(data), headers=headers, content_type="application/json")
     assert response.status_code == 404
     assert response.get_json()["error"] == "Project not found"
 
 
-def test_update_project_invalid_id(client):
+def test_update_project_invalid_id(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     data = {"name": "Invalid ID"}
-    response = client.put("/api/projects/abc", data=json.dumps(data), content_type="application/json")
+    response = client.put("/api/projects/abc", data=json.dumps(data), headers=headers, content_type="application/json")
     assert response.status_code == 400
     assert response.get_json()["error"] == "Invalid ID format"
 
 
 def test_delete_project_success(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     project = Project(name="Delete Project", description="desc")
     db_session.add(project)
     db_session.commit()
 
-    response = client.delete(f"/api/projects/{project.id}")
+    response = client.delete(f"/api/projects/{project.id}", headers=headers)
     assert response.status_code == 200
     assert db_session.get(Project, project.id) is None
 
 
-def test_delete_project_not_found(client):
-    response = client.delete("/api/projects/999")
+def test_delete_project_not_found(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.delete("/api/projects/999", headers=headers)
     assert response.status_code == 404
     assert response.get_json()["error"] == "Project not found"
 
 
-def test_delete_project_invalid_id(client):
-    response = client.delete("/api/projects/abc")
+def test_delete_project_invalid_id(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.delete("/api/projects/abc", headers=headers)
     assert response.status_code == 400
     assert response.get_json()["error"] == "Invalid ID format"
 
 
 def test_create_task_under_project(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     project = Project(name="Project #1", description="desc")
     db_session.add(project)
     db_session.commit()
@@ -128,7 +161,7 @@ def test_create_task_under_project(client, db_session):
     }
     response = client.post(
         f"/api/projects/{project.id}/tasks",
-        data=json.dumps(data),
+        data=json.dumps(data), headers=headers,
         content_type="application/json"
     )
 
@@ -143,6 +176,7 @@ def test_create_task_under_project(client, db_session):
 
 
 def test_create_task_missing_field(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     project = Project(name="Project #1", description="desc")
     db_session.add(project)
     db_session.commit()
@@ -150,7 +184,7 @@ def test_create_task_missing_field(client, db_session):
     data = {"description": "Missing title"}
     response = client.post(
         f"/api/projects/{project.id}/tasks",
-        data=json.dumps(data),
+        data=json.dumps(data), headers=headers,
         content_type="application/json"
     )
     assert response.status_code == 400
@@ -158,6 +192,7 @@ def test_create_task_missing_field(client, db_session):
 
 
 def test_get_tasks_under_project(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     project = Project(name="Project #1", description="desc")
     db_session.add(project)
     db_session.commit()
@@ -165,21 +200,23 @@ def test_get_tasks_under_project(client, db_session):
     db_session.add(Task(title="Task #1", description="desc", project_id=project.id))
     db_session.commit()
 
-    response = client.get(f"/api/projects/{project.id}/tasks")
+    response = client.get(f"/api/projects/{project.id}/tasks", headers=headers)
     assert response.status_code == 200
     payload = response.get_json()
-    assert isinstance(payload, list)
-    assert len(payload) == 1
-    assert payload[0]["title"] == "Task #1"
+    assert isinstance(payload['data'], list)
+    assert len(payload['data']) == 1
+    assert payload['data'][0]["title"] == "Task #1"
 
 
-def test_get_tasks_invalid_project(client):
-    response = client.get("/api/projects/999/tasks")
+def test_get_tasks_invalid_project(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.get("/api/projects/999/tasks", headers=headers)
     assert response.status_code == 404
     assert response.get_json()["error"] == "Project not found"
 
 
-def test_get_tasks_invalid_id_format(client):
-    response = client.get("/api/projects/abc/tasks")
+def test_get_tasks_invalid_id_format(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.get("/api/projects/abc/tasks", headers=headers)
     assert response.status_code == 400
     assert response.get_json()["error"] == "Invalid ID format"
--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -2,17 +2,39 @@
 import json
 
 from app.models.users import User
+from app.routes.auth import create_auth_token
 
+def get_token(session):
+
+    manager_user = session.query(User).filter_by(email="manager@example.com").first()
+    if not manager_user:
+        manager_user = User(
+            first_name="Manager",
+            last_name="User",
+            email="manager@example.com",
+            role="manager",
+            password="SecureP@ssword1"
+        )
+        session.add(manager_user)
+        session.commit()
+
+    token = create_auth_token(manager_user)
+
+    return token
 
 def test_create_user(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    
     data = {
         "first_name": "Test",
         "last_name": "User",
         "email": "testuser@example.com",
-        "role": "employee"
+        "role": "employee",
+        "password": "SecureP@ssword1",
+        "confirm_password": "SecureP@ssword1"
     }
 
-    response = client.post("/api/users", data=json.dumps(data), content_type="application/json")
+    response = client.post("/api/users", data=json.dumps(data), headers=headers, content_type="application/json")
 
     assert response.status_code == 201
     payload = response.get_json()
@@ -29,23 +51,29 @@ def test_create_user(client, db_session):
     assert user.role.value == "employee"
 
 
-def test_create_user_no_field(client):
+def test_create_user_no_field(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     data = {
         "first_name": "Test",
         "last_name": "User",
-        "role": "manager"
+        "role": "manager",
+        "email": "testuser@example.com",
+        "confirm_password": "SecureP@ssword1"
     }
-    response = client.post("/api/users", data=json.dumps(data), content_type="application/json")
+    response = client.post("/api/users", data=json.dumps(data), headers=headers, content_type="application/json")
     assert response.status_code == 400
     assert "Missing field" in response.get_json()["error"]
 
 
 def test_create_user_unique(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+
     user = User(
         first_name="Test", 
         last_name="User", 
         email="testing@example.com", 
-        role="employee"
+        role="employee",
+        password="SecureP@ssword1"
     )
     db_session.add(user)
     db_session.commit()
@@ -54,45 +82,53 @@ def test_create_user_unique(client, db_session):
         "first_name": "Test",
         "last_name": "User",
         "email": "testing@example.com",
-        "role": "manager"
+        "role": "manager",
+        "password": "SecureP@ssword1",
+        "confirm_password": "SecureP@ssword1"
     }
 
-    response = client.post("/api/users", data=json.dumps(data), content_type="application/json")
+    response = client.post("/api/users", data=json.dumps(data), headers=headers, content_type="application/json")
     assert response.status_code == 400
     assert "Email already exists" in response.get_json()["error"]
 
 
 def test_list_users_returns_all(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+
     db_session.add_all([
         User(
             first_name="User", 
             last_name="Test", 
             email="usertest@example.com", 
-            role="manager"
+            role="manager",
+            password="SecureP@ssword1"
         ),
     ])
     db_session.commit()
 
-    response = client.get("/api/users/")
+    response = client.get("/api/users/", headers=headers)
     assert response.status_code == 200
     payload = response.get_json()
-    assert isinstance(payload, list)
-    assert len(payload) >= 1
-    emails = [user["email"] for user in payload]
+    assert "data" in payload
+    assert isinstance(payload['data'], list)
+    assert len(payload['data']) >= 1
+    emails = [user["email"] for user in payload['data']]
     assert "usertest@example.com" in emails
 
 
 def test_get_user(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     user = User(
         first_name="Test", 
         last_name="User",
         email="testuser+1@example.com", 
-        role="employee"
+        role="employee",
+        password="SecureP@ssword1"
     )
     db_session.add(user)
     db_session.commit()
 
-    response = client.get(f"/api/users/{user.id}")
+    response = client.get(f"/api/users/{user.id}", headers=headers)
     assert response.status_code == 200
     payload = response.get_json()
     assert payload["first_name"] == "Test"
@@ -101,24 +137,28 @@ def test_get_user(client, db_session):
     assert payload["role"] == "employee"
 
 
-def test_get_user_not_found(client):
-    response = client.get("/api/users/100")
+def test_get_user_not_found(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.get("/api/users/100", headers=headers)
     assert response.status_code == 404
     assert response.get_json()["error"] == "User not found"
 
 
-def test_get_user_invalid_id(client):
-    response = client.get("/api/users/abc")
+def test_get_user_invalid_id(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.get("/api/users/abc", headers=headers)
     assert response.status_code == 400
     assert response.get_json()["error"] == "Invalid ID format"
 
 
 def test_update_user_success(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     user = User(
         first_name="juan", 
         last_name="Test", 
         email="juantest@example.com", 
-        role="employee"
+        role="employee",
+        password="SecureP@ssword1"
     )
     db_session.add(user)
     db_session.commit()
@@ -127,6 +167,7 @@ def test_update_user_success(client, db_session):
     response = client.put(
         f"/api/users/{user.id}",
         data=json.dumps(data),
+        headers=headers,
         content_type="application/json"
     )
 
@@ -139,21 +180,25 @@ def test_update_user_success(client, db_session):
     assert updated.role.value == "manager"
 
 
-def test_update_user_not_found(client):
+def test_update_user_not_found(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     data = {"first_name": "X"}
     response = client.put(
         "/api/users/100", 
-        data=json.dumps(data), 
+        data=json.dumps(data),
+        headers=headers,
         content_type="application/json"
     )
     assert response.status_code == 404
     assert response.get_json()["error"] == "User not found"
 
 
-def test_update_user_invalid_id(client):
+def test_update_user_invalid_id(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     response = client.put(
         "/api/users/abc", 
         data=json.dumps({"first_name": "Testing"}), 
+        headers=headers,
         content_type="application/json"
     )
     assert response.status_code == 400
@@ -161,28 +206,32 @@ def test_update_user_invalid_id(client):
 
 
 def test_delete_user_success(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
     user = User(
         first_name="Flask", 
         last_name="API",
         email="flasapi@example.com", 
-        role="employee"
+        role="employee",
+        password="SecureP@ssword1"
     )
     db_session.add(user)
     db_session.commit()
 
-    response = client.delete(f"/api/users/{user.id}")
+    response = client.delete(f"/api/users/{user.id}", headers=headers)
     assert response.status_code == 200
     deleted = db_session.get(User, user.id)
     assert deleted is None
 
 
-def test_delete_user_not_found(client):
-    response = client.delete("/api/users/100")
+def test_delete_user_not_found(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.delete("/api/users/100", headers=headers)
     assert response.status_code == 404
     assert response.get_json()["error"] == "User not found"
 
 
-def test_delete_user_invalid_id(client):
-    response = client.delete("/api/users/abc")
+def test_delete_user_invalid_id(client, db_session):
+    headers = {"Authorization": f"Bearer {get_token(db_session)}"}
+    response = client.delete("/api/users/abc", headers=headers)
     assert response.status_code == 400
     assert response.get_json()["error"] == "Invalid ID format"
